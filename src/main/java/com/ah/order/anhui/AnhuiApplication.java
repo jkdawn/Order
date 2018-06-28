abb99ffdb550d7609dc570356cfcf06a96b88ff0
zhougit <408100209@qq.com>
2018-06-28 20:40:05 +0800

    项目模型

---
+++ src/main/java/com/ah/order/anhui/AnhuiApplication.java
@@ -0,0 +1,12 @@
+package com.ah.order.anhui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnhuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnhuiApplication.class, args);
+	}
+}
